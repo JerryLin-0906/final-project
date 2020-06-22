aefcbc66da11d1c7d8f48cb710b8a889c675af6c
JerryLin-0906 <64945645+JerryLin-0906@users.noreply.github.com>
2020-06-22 17:24:06 +0800

    Add files via upload

--- test.js
+++ test.js
@@ -4,64 +4,51 @@ function goToAnchor(anchor) {
   document.location = loc + '#' + anchor;
   return false;
 }
-// Create a "close" button and append it to each list item
-/*var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-*/
 // Create a new list item when clicking on the "Add" button
 function newElement() {
   var div = document.createElement("div");
   var inputValue = document.getElementById("myInput").value;
+  var class1 = document.getElementById('class1').innerText
+  var class2 = document.getElementById('class2').innerText
+  var class3 = document.getElementById('class3').innerText
+  var divchild = document.createElement('div')
+  var a = document.createElement('a')
   var t = document.createTextNode(inputValue);
-  div.appendChild(t);
-  if (inputValue === '') {
-    alert("請填入餐廳名稱");
+  //div.appendChild(t);
+  div.appendChild(divchild);
+  div.appendChild(a);
+  a.appendChild(t);
+  if (inputValue === '' || class1 === '在哪裡?' || class2 === '賣什麼?') {
+    alert("請填入餐廳名稱、餐廳地點或餐廳種類");
   } else {
     document.getElementById("restaurants").appendChild(div);
-    AddClass(div,"filterDiv show1")
+    AddClass(div,"filterDiv filtered show1")
+    AddClass(div,class1)
+    AddClass(div,class2)
+    if (class3 === '還有賣什麼?'){
+      AddClass(div,'')
+    }else {
+      AddClass(div,class3)
+    }
+  AddClass(divchild,'delete')
+  divchild.onclick = unshow
   }
   document.getElementById("myInput").value = "";
+  document.getElementById('class1').innerText = '在哪裡?';
+  document.getElementById('class2').innerText = '賣什麼?';
+  document.getElementById('class3').innerText = '還有賣什麼?';
 }
-/*
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
 
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+function change(id,name) {
+  var x = document.getElementById(id);
+  x.innerText = name
 }
-*/
+
+function unshow() {
+  delete event.target.parentElement.remove()
+}
+
 function draw() {
   var x, array, j;
   x = document.getElementsByClassName("show1");
